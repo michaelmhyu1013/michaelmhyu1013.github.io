004cf1201ace8ef229b91d1fa2f73f3e6fa90ba0
michaelmhyu1013 <michaelmhyu94@gmail.com>
2020-01-28 20:47:30 -0800

    Update styling for list

--- js/list.js
+++ js/list.js
@@ -1,70 +1,68 @@
-const toggleAddArtistModal = data => {
+const toggleAddArtistModal = () => {
     const addArtistForm = document.querySelector(".add-artist-form");
-    addArtistForm.style.display =
-        addArtistForm.style.display === "block" ? "none" : "block";
+    toggleModal(addArtistForm);
     addArtistForm.firstElementChild.reset();
 };
 
 const addArtist = () => {
-    console.log("Adding artist");
+    const addArtistForm = document.querySelector(".add-artist-form");
     const artistName = document.getElementById("artist-name").value;
+    if (!artistName) {
+        return;
+    }
     const artistAbout = document.querySelector("#artist-about").value;
     const artistImageURL = document.querySelector("#artist-image").value;
     const artistList = document.querySelector(".list-avatar");
 
     let avatar = document.createElement("div");
+    let avatarID = Date.now();
+    avatar.setAttribute("id", avatarID);
     avatar.classList.add("avatar");
 
-    let artistMarkup = `
-                    <div class="avatar-img">
-                        <img
-                            src="${artistImageURL}"
-                        />
-                    </div>
-                    <div class="avatar-description">
-                        <div class="avatar-name">
-                            <p>
-                                ${artistName}
-                            </p>
-                        </div>
-                        <div class="avatar-location">
-                            <p>
-                                ${artistAbout};
-                            </p>
-                        </div>
-                    </div>
-                    <div class="btn-delete" >
-                        <button type="button">Delete</button>
-                    </div>
-    `;
+    let newArtistImg = document.createElement("div");
+    newArtistImg.classList.add("avatar-img");
+    let image = document.createElement("img");
+    image.setAttribute("src", artistImageURL);
 
+    newArtistImg.appendChild(image);
 
+    let newArtistDescription = document.createElement("div");
+    newArtistDescription.classList.add("avatar-description");
 
-    // let newArtist = document.createElement("div");
-    // newArtist.classList.add("avatar");
+    let name = document.createElement("div");
+    name.classList.add("avatar-name");
+    name.textContent = artistName;
 
-    // let newArtistImg = document.createElement("div");
-    // newArtistImg.classList.add("avatar-img");
-    // let image = document.createElement("img");
+    let about = document.createElement("div");
+    about.classList.add("avatar-location");
+    about.textContent = artistAbout;
 
-    // newArtistImg.appendChild(image);
+    let deleteContainer = document.createElement("div");
+    deleteContainer.classList.add("btn-delete");
+    let deleteButton = document.createElement("button");
+    deleteButton.setAttribute("type", "button");
+    deleteButton.setAttribute("onclick", `deleteArtist(${avatarID})`);
+    deleteButton.textContent = "Delete";
+    deleteContainer.appendChild(deleteButton);
 
-    // let newArtistDescription = document.createElement("div");
-    // newArtistDescription.classList.add("avatar-description");
-    // let name = document.createElement("div");
-    // name.classList.add("avatar-name");
-    // let about = document.createElement("div");
-    // about.classList.add("avatar-location");
+    newArtistDescription.appendChild(name);
+    newArtistDescription.appendChild(about);
 
-    // newArtistDescription.appendChild(name);
-    // newArtistDescription.appendChild(about);
+    avatar.appendChild(newArtistImg);
+    avatar.appendChild(newArtistDescription);
+    avatar.appendChild(deleteContainer);
 
-    // newArtist.appendChild(newArtistImg);
-    // newArtist.appendChild(newArtistDescription);
-
-
-    avatar.innerHTML = artistMarkup;
+    toggleModal(addArtistForm);
+    addArtistForm.firstElementChild.reset();
     artistList.appendChild(avatar);
 };
 
-const deleteArtist = () => {};
+const deleteArtist = id => {
+    let avatarList = document.querySelector(".list-avatar");
+    let avatarToDelete = document.getElementById(id);
+    avatarList.removeChild(avatarToDelete);
+};
+
+const toggleModal = elem => {
+    elem.style.display = elem.style.display === "block" ? "none" : "block";
+};
