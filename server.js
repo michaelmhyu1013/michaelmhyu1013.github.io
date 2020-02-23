344c7ebcd8e497bfdda3c3cfb6a7264ef99c7b4f
michaelmhyu1013 <michaelmhyu94@gmail.com>
2020-02-23 01:33:40 -0800

    Update button styling

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require("body-parser");
 var path = require("path");
 var app = express();
 
-const port = 3000;
+const port = 8000;
 const testRoutes = require("./routes/testRoutes");
 
 app.use(bodyParser.json());
